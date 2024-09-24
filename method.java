079bebd7a5a217df5c4105298807b4884a750d6e
2382032 <2382032@unai.edu>
2024-09-24 10:17:16 +0700

    handson4

---
+++ method.java
@@ -0,0 +1,14 @@
+public class method {
+    public static int hitungluaspersegi(int sisi) {
+        int luas = sisi + sisi;
+        return luas;
+
+    }
+
+    public static void main(String[] args) {
+        int sisi = 5;
+        int luas = hitungluaspersegi(sisi);
+        System.out.println("Luas persegi dengan sisi" + "adalah" + luas);
+
+    }
+}
